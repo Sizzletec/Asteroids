dff88758476dfec007203e296b7b1e9161c2c3a0
Adam Emery <adam.emery@accenturefederal.com>
2016-12-12 14:22:12 -0500

    burst fire bounce

--- components/ship/BounceComponent.lua
+++ components/ship/BounceComponent.lua
@@ -5,22 +5,36 @@ BounceComponent.__index = BounceComponent
 function BounceComponent.new(entity)
   local i = {
     gunCooldown = 0,
-    weaponDamage = 30,
-    fireRate = 2,
-    firing = false
+    weaponDamage = 15,
+    fireRate = .8,
+    firing = false,
+    burstOffset = 0.1
   }
 
   setmetatable(i, BounceComponent)
   i.entity = entity
+
+  i.burst = 0
+  i.burstCooldown = 0
   return i
 end
 
 function BounceComponent:update(dt)
   if self.firing and self.gunCooldown <= 0 then
     self:fire()
+    self.burst = 2
+    self.burstCooldown = self.burstOffset
     self.gunCooldown = 1/self.fireRate
   elseif self.gunCooldown > 0 then
     self.gunCooldown = self.gunCooldown - dt
+  end
+
+  self.burstCooldown = self.burstCooldown  - dt
+
+  if self.burst > 0  and self.burstCooldown <= 0 then
+    self.burst = self.burst -1
+    self.burstCooldown = self.burstOffset
+    self:fire()
   end
 end
 
